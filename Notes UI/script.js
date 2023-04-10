620a258b3f5c18c527d8f179a500515f061be607
dezoliveira <andresoliveira@protonmail.com>
2023-04-10 17:36:20 -0300

    Allow Corse and Create and List Notes

---
+++ Notes UI/script.js
@@ -0,0 +1,52 @@
+const saveButton = document.querySelector("#btnSave")
+const titleInput = document.querySelector('#title')
+const descriptionInput = document.querySelector('#description')
+const notesContainer = document.querySelector('#notes__container')
+
+function addNote(title, description) {
+
+  const body = {
+    title: title,
+    description: description,
+    isVisible: true
+  }
+
+  fetch('https://localhost:7124/api/notes', {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      "content-type": "application/json"
+    }
+  })
+  .then(data => data.json())
+  .then(response => console.log(response))
+}
+
+function displayNotes(notes) {
+  let allNotes = ''
+
+  notes.forEach(note => {
+    const noteElement = `
+                          <div class="note">
+                            <h3>${note.title}</h3>
+                            <p>${note.description}</p>
+                          </div>
+                        `
+    allNotes += noteElement
+  });
+
+  notesContainer.innerHTML = allNotes
+}
+
+function getAllNotes() {
+  fetch('https://localhost:7124/api/notes')
+  .then(data => data.json())
+  .then(response => displayNotes(response))
+}
+
+getAllNotes()
+
+saveButton.addEventListener('click', function(){
+    addNote(titleInput.value, descriptionInput.value)
+  }
+)
